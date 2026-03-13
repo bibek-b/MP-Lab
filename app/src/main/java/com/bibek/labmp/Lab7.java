2604f82a1cf54ebec70f7765db5adeff147cd611
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-13 10:07:09 +0545

    some changes

--- app/src/main/java/com/bibek/labmp/Lab7.java
+++ app/src/main/java/com/bibek/labmp/Lab7.java
@@ -1,17 +1,22 @@
 package com.bibek.labmp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class Lab7 extends AppCompatActivity {
 
     Button btnShowAns;
+    RadioGroup rg1,rg2,rg3,rg4, rg6;
+    EditText etQ5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,40 @@ public class Lab7 extends AppCompatActivity {
         setContentView(R.layout.activity_lab7);
 
         btnShowAns = findViewById(R.id.btn_showAns);
+        rg1 = findViewById(R.id.rg_q1Options);
+        rg2 = findViewById(R.id.rg_q2Options);
+        rg3 = findViewById(R.id.rg_q3Options);
+        rg4 = findViewById(R.id.rg_q4Options);
+        rg6 = findViewById(R.id.rg_q6Options);
+
+        etQ5 = findViewById(R.id.et_q5);
+
+        btnShowAns.setOnClickListener(v->{
+            String q1 = getRadioValue(rg1);
+            String q2 = getRadioValue(rg2);
+            String q3 = getRadioValue(rg3);
+            String q4 = getRadioValue(rg4);
+            String q6 = getRadioValue(rg6);
+            String q5 = etQ5.getText().toString();
+
+            Intent intent = new Intent(this, Show7Ans.class);
+
+            intent.putExtra("q1", q1);
+            intent.putExtra("q2", q2);
+            intent.putExtra("q3", q3);
+            intent.putExtra("q4", q4);
+            intent.putExtra("q5", q5);
+            intent.putExtra("q6", q6);
+
+            startActivity(intent);
+        });
+    }
+    private String getRadioValue(RadioGroup rg){
+        int id = rg.getCheckedRadioButtonId();
+        if(id == -1) return  "Not answered";
+
+        RadioButton rb = findViewById(id);
+        return  rb.getText().toString();
+
     }
 }
--- app/src/main/java/com/bibek/labmp/Show7Ans.java
+++ app/src/main/java/com/bibek/labmp/Show7Ans.java
@@ -1,24 +1,34 @@
 package com.bibek.labmp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class Show7Ans extends AppCompatActivity {
+    TextView tv1, tv2, tv3, tv4, tv5, tv6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_show7_ans);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        tv1 = findViewById(R.id.ans1);
+        tv2 = findViewById(R.id.ans2);
+        tv3 = findViewById(R.id.ans3);
+        tv4 = findViewById(R.id.ans4);
+        tv5 = findViewById(R.id.ans5);
+        tv6 = findViewById(R.id.ans6);
+
+        tv1.setText(getIntent().getStringExtra("q1"));
+        tv2.setText(getIntent().getStringExtra("q2"));
+        tv3.setText(getIntent().getStringExtra("q3"));
+        tv4.setText(getIntent().getStringExtra("q4"));
+        tv5.setText(getIntent().getStringExtra("q5"));
+        tv6.setText(getIntent().getStringExtra("q6"));
+
     }
 }
